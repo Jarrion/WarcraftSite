4de32b03a931a04a31efb802e1dbecd5943d1eb2
Jarrion <jaredhunt2217@gmail.com>
2023-01-20 14:57:44 -0500

    added dungeons.js

---
+++ scripts/dungeons.js
@@ -0,0 +1,14 @@
+//Expansions are listed in the order they came out, and each dungeon is listed in the order that they appear in the dungeon journal in-game
+
+const dungeonArrays = {
+    vanilla: [blackfathomDeeps, blackrockDepths, deadmines, direMaul, gnomeregan, lowerBlackrockSpire, maraudon, ragefireChasm, razorfenDowns, razorfenKraul, scarletHalls, scarletMonastery, scholomance, shadowfangKeep, stratholme, theStockade, sunkenTemple, uldaman, wailingCaverns, zulFarrak],
+    theBurningCrusade: [],
+    wrathOfTheLichKing: [],
+    cataclysm: [],
+    mistsOfPandaria: [],
+    warlordsOfDraenor: [],
+    legion: [],
+    battleForAzeroth: [],
+    shadowlands: [],
+    dragonflight: []
+}
